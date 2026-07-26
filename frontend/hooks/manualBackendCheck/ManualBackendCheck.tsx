fd4647b7dc27b4236429b9674c32278bc8d07316
Abhishek Jatav <abhidel44@gmail.com>
2026-07-26 18:59:11 +0530

    improvements

--- frontend/hooks/manualBackendCheck/ManualBackendCheck.tsx
+++ frontend/hooks/manualBackendCheck/ManualBackendCheck.tsx
@@ -26,8 +26,9 @@ export default function ManualBackendCheck() {
   const [cooldown, setCooldown] = useState(0);
   const [statusText, setStatusText] = useState("Backend Sleeping");
 
+  // ⏱️ 30-second cooldown
   const startCooldown = () => {
-    setCooldown(60);
+    setCooldown(30);
   };
 
   const wakeBackend = async () => {
@@ -128,24 +129,3 @@ export default function ManualBackendCheck() {
     </div>
   );
 }
-
-// page
-
-// import ReactionTestGame from "@/hooks/backendCheck/game/reactionTest/reactionTestGame";
-// import ManualBackendCheck from "../hooks/manualBackendCheck/ManualBackendCheck";
-
-// export default function BackendCheckPage() {
-//   return (
-//     <div className="relative w-full min-h-screen bg-[#020617] overflow-hidden">
-//       {/* Floating Top-Left Panel */}
-//       <div className="absolute top-4 left-4 z-10 w-[90%] sm:w-[350px] md:w-[400px] bg-[#0f172a] rounded-2xl p-4 shadow-xl">
-//         <ManualBackendCheck />
-//       </div>
-
-//       {/* Full Screen Background Component */}
-//       <div className="w-full h-screen flex items-center justify-center">
-//         <ReactionTestGame />
-//       </div>
-//     </div>
-//   );
-// }
